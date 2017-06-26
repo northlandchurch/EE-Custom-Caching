79618d7f5aec496de746577cef133cc6f291a544
Will McAuliff <womcauliff@gmail.com>
2017-06-26 11:43:17 -0400

    Fixes GET param conditional. Restructures response JSON. Formats comments.

--- remove_EE_Cache.php
+++ remove_EE_Cache.php
@@ -1,5 +1,6 @@
 <?php
-	require_once('./http_response_code.php');
+
+	require_once('/var/www/ee/scripts/keephpcache/http_response_code.php');
 
 	/**
 	 * Sets Access-Control-Allow-Origin for any requests originating from
@@ -8,69 +9,64 @@
 	$http_origin = $_SERVER['HTTP_ORIGIN'];
 	if ($http_origin == "http://duzzit.northlandchurch.org")
 	{
-	   header("Access-Control-Allow-Origin: $http_origin");
+		header("Access-Control-Allow-Origin: $http_origin");
 	}
 
-	$json = array();//Initialize the JSON response that will be sent back to client
+	$json = array();// Initialize the JSON response that will be sent back to client
 
-	// check for each of the required GET parameters
-	if(!empty($_POST) || isset($_POST["emailaddress"]) {
-		//import custom caching library
+	// Check for each of the required POST parameters
+	if(!empty($_POST) && isset($_POST["url"])) {
+
+		// Import custom caching library
 		require_once ('/var/www/ee/scripts/keephpcache/KeePHPCache.php');
 		$cache = new KeePHPCache();
 
-		//confirm that cache already exists
+		// Confirm that cache already exists
 		if($cache->cacheExists($_POST["url"])){
 
-			//cached file removal succeeds
+			// Cached file removal succeeds
 			if($cache->removeCacheFile($_POST["url"])) {
 				http_response_code(200);
-				$json = array(
-					array(
-						"msg" => "Cached file removed."
-					)
+				$json["result"] = "success";
+				$json["msg"] = "Cached file removed.";
+				$json["status"] = 200;
+				$json["params_received"] = array(
+					"url" => $_POST["url"]
 				);
 			}
-			//cached file removal fails
+			// Cached file removal fails
 			else {
 				http_response_code(500);
-				$json = array(
-					array(
-						"error" => "Cached file could not be removed.",
-						"params_received" => array(
-								"url" => $_POST["url"]
-						)
-					)
+				$json["result"] = "error";
+				$json["status"] = 500;
+				$json["params_received"] = array(
+					"url" => $_POST["url"]
 				);
 			}
 		}
-		// cached file does not exist
+		// Cached file does not exist
 		else {
 			http_response_code(404);
-			$json = array(
-				array(
-					"error" => "Cached file not found.",
-					"params_received" => array(
-							"url" => $_POST["url"]
-					)
-				)
+			$json["result"] = "error";
+			$json["msg"] = "Cached file not found.";
+			$json["status"] = 404;
+			$json["params_received"] = array(
+				"url" => $_POST["url"]
 			);
 		}
 	}
 	// Missing one or more required parameters
 	else {
 		http_response_code(400);
-		$json = array(
-			array(
-			"error" => "Missing one or more required parameters.",
-			"params_received" => array(
-					"url" => $_POST["url"]
-				)
-			)
+		$json["result"] = "error";
+		$json["msg"] = "Missing one or more required parameters.";
+		$json["status"] = 400;
+		$json["params_received"] = array(
+			"url" => $_POST["url"]
 		);
 	}
 
-	// send response back to script and end script execution
+	// Send response back to script and end script execution
 	header("Content-Type: application/json");
 	$encoded = json_encode($json);
 	die($encoded);
